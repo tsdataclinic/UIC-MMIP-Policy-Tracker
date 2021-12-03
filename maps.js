14a4c09bd2e5d9a7768bdea296bf0b39f2e70387
Two Sigma <kmohan@twosigma.com>
2021-12-03 10:00:20 -0500

    adding us states layer and basic popup

--- maps.js
+++ maps.js
@@ -7,6 +7,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaW1hZGF2ZWxvcGVyIiwiYSI6ImNrZTdxZ3RiNjBydnYyc
 //   [-95.3566105298246, 13.0517966258875], // Southwest coordinates, , 
 //   [-85.2111927824501, 18.416632999923] // Northeast coordinates, 
 // ];
+$.getJSON("data/us_states.geojson", function(jsonData) { 
+  states_geojson = jsonData
+  console.log(states_geojson)
+});
 
 var map = new mapboxgl.Map({
   container: 'map', // container id
@@ -23,15 +27,36 @@ map.addControl(new mapboxgl.NavigationControl({
 
 map.on('load', function() {
 
-  map.addSource('usmap', {
-    type: 'geojson',
-    data: states_geojson
+  // Add a source for the state polygons.
+  map.addSource('states', {
+  'type': 'geojson',
+  'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/ne_110m_admin_1_states_provinces_shp.geojson'
   });
   
   resetMap = document.getElementById('reset_button')
   resetMap.addEventListener('click', function(){
     map.flyTo({center: [-104.6597064,37.275387], zoom:3});
   })
+
+  map.addLayer({
+    'id': 'states-layer',
+    'type': 'fill',
+    'source': 'states',
+    'layout':{"visibility": 'visible'},
+    "paint": {
+      "fill-color":'#193a45',
+      "fill-opacity": 0.1,
+      "fill-outline-color": "black"
+  }
+  });
+
+
+  map.on('click', 'states-layer', (e) => {
+    new mapboxgl.Popup({closeButton: false})
+    .setLngLat(e.lngLat)
+    .setHTML(e.features[0].properties.name)
+    .addTo(map);
+    });
 
 });
 
